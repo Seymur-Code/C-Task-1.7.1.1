1ea1f6f812a07bf9b06d0bb8ae06395138cc3fdf
Seymur-Code <tu201806001@code.edu.az>
2021-11-04 20:11:05 +0400

    Add project files.

---
+++ C-Task-1.7.1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_Task_1._7._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arry1 = { 7, 8, 3, 4, 5, 6 };
+            Console.WriteLine("please enter the number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(Class1.argss(Arry1, a));
+        }
+    }
+}
---
+++ NewClass/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NewClass
+{
+    public class Class1
+    {
+        public static int argss(int[] Arry1, int a)
+        {
+            for (int i = 0; i < Arry1.Length; i++)
+            {
+                if (a == Arry1[i])
+                {
+                    Console.WriteLine($"{a}");
+
+                }
+            }
+            return a;
+        }
+    }
+}
